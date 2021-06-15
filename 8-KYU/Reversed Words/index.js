fbe3a8c99535112ba92c607364c7e8bbe06db853
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-06-15 08:22:24 +0700

    Reversed Words 15/6/21

---
+++ 8-KYU/Reversed Words/index.js
@@ -0,0 +1,7 @@
+// function reverseWords(str) {
+// 	return str.split(" ").reverse().join(" ");
+// }
+
+const reverseWords = (str) => str.split(" ").reverse().join(" ");
+
+console.log(reverseWords("Tes Awal Akhir"));
