ec045fdde044522a974aef8330a3c04ec196a218
Dimas Andhika Diputra <dimasandhikadiputra@gmail.com>
2021-06-15 08:37:13 +0700

    Sum Strings as Numbers 15/6/21

---
+++ 4-KYU/Sum Strings as Numbers/index.js
@@ -0,0 +1,42 @@
+// function sumStrings(a, b) {
+// 	return `${BigInt(a) + BigInt(b)}`;
+// }
+
+// const _0x2240 = [
+// 	"284426DzqJrg",
+// 	"148123MBXekq",
+// 	"129656zFlRTc",
+// 	"1mbQKMt",
+// 	"136327kJrbAr",
+// 	"75365JPsXiy",
+// 	"123557NBXYjM",
+// 	"98688SGktsO"
+// ];
+// (function (_0x567ecb, _0x500e25) {
+// 	const _0x38b394 = _0x2123;
+// 	while (!![]) {
+// 		try {
+// 			const _0xd299ed =
+// 				parseInt(_0x38b394(0x19a)) * parseInt(_0x38b394(0x19c)) +
+// 				parseInt(_0x38b394(0x19f)) +
+// 				parseInt(_0x38b394(0x19e)) +
+// 				-parseInt(_0x38b394(0x199)) +
+// 				parseInt(_0x38b394(0x19d)) +
+// 				-parseInt(_0x38b394(0x19b)) +
+// 				parseInt(_0x38b394(0x1a0));
+// 			if (_0xd299ed === _0x500e25) break;
+// 			else _0x567ecb["push"](_0x567ecb["shift"]());
+// 		} catch (_0x2bbe64) {
+// 			_0x567ecb["push"](_0x567ecb["shift"]());
+// 		}
+// 	}
+// })(_0x2240, 0x2902a);
+// function _0x2123(_0x457081, _0x53bf32) {
+// 	_0x457081 = _0x457081 - 0x199;
+// 	let _0x2240c2 = _0x2240[_0x457081];
+// 	return _0x2240c2;
+// }
+// const sumStrings = (_0x33bc61, _0x40f332) => "" + (BigInt(_0x33bc61) + BigInt(_0x40f332));
+
+const sumStrings = (a, b) => `${BigInt(a) + BigInt(b)}`;
+console.log(sumStrings("1", "2"));
